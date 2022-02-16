134ca8f3db0b1780415fa2d6d96bb0923c466179
denniskuipersSN <dennis.kuipers@servicenow.com>
2022-02-16 10:55:42 +0100

    Add Jenkins 5

--- git/jenkins/install_jenkins.sh
+++ git/jenkins/install_jenkins.sh
@@ -1,30 +1,30 @@
-#/bin/bash
+#!/bin/bash
 #Create name space for Jenkins
-kubectl create namespace jenkins
+/usr/local/bin/kubectl create namespace jenkins
 
 #Get the chart for Jenkins
-helm repo add jenkinsci https://charts.jenkins.io
-helm repo update
+/usr/local/bin/helm repo add jenkinsci https://charts.jenkins.io
+/usr/local/bin/helm repo update
 
 #Create persistence volume for Jenkins
-kubectl apply -f jenkins-volume.yaml
+/usr/local/bin/kubectl apply -f /home/ec2-user/git/jenkins/jenkins-volume.yaml
 #Create Account for Jenkins
-kubectl apply -f jenkins-sa.yaml
+/usr/local/bin/kubectl apply -f /home/ec2-user/git/jenkins/jenkins-sa.yaml
 
 #Create Jenkins Container
 chart=jenkinsci/jenkins
-helm install jenkins -n jenkins -f jenkins-values.yaml $chart
+/usr/local/bin/helm install jenkins -n jenkins -f /home/ec2-user/git/jenkins/jenkins-values.yaml $chart
 
 #Password for Jenkins
 jsonpath="{.data.jenkins-admin-password}"
-secret=$(kubectl get secret -n jenkins jenkins -o jsonpath=$jsonpath)
+secret=$(/usr/local/bin/kubectl get secret -n jenkins jenkins -o jsonpath=$jsonpath)
 echo $(echo $secret | base64 --decode)
 jsonpath="{.spec.ports[0].nodePort}"
-NODE_PORT=$(kubectl get -n jenkins -o jsonpath=$jsonpath services jenkins)
+NODE_PORT=$(/usr/local/bin/kubectl get -n jenkins -o jsonpath=$jsonpath services jenkins)
 jsonpath="{.items[0].status.addresses[0].address}"
-NODE_IP=$(kubectl get nodes -n jenkins -o jsonpath=$jsonpath)
+NODE_IP=$(/usr/local/bin/kubectl get nodes -n jenkins -o jsonpath=$jsonpath)
 echo http://$NODE_IP:$NODE_PORT/login
 
 #Port forward
-pod_name=kubectl -n jenkins get --no-headers=true pods -o name | awk -F "/" '{print $2}'
-kubectl -n jenkins port-forward $pod_name 8080:8080
+pod_name=/usr/local/bin/kubectl -n jenkins get --no-headers=true pods -o name | awk -F "/" '{print $2}'
+/usr/local/bin/kubectl -n jenkins port-forward $pod_name 8080:8080
